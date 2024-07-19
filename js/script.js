07b558f1efe4876bfc3e9d5fc2de7824a78fd758
gouoy <ga0cord@gmail.com>
2024-07-19 12:28:32 +0900

    1230pm 240719

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 /* 초기 로딩 작업 _____________________________________________ */
 document.onreadystatechange = function() {
+
   if (document.readyState !== "complete") {
       document.getElementById('wrap').style.visibility = "hidden";
       document.querySelector(
-        "#loading").style.visibility = "visible";
+        "#preloader").style.visibility = "visible";
   } else {
       document.querySelector(
-        "#loading").style.display = "none";
+        "#preloader").style.display = "none";
       document.querySelector(
         "#wrap").style.visibility = "visible";
   }
 };
+
+function counter(){
+  const count = setInterval(function(){
+    const c = document.getElementById("counter"),
+    int = parseInt(c.textContent); 
+    c.textContent = (++int).toString();
+    if(int == 100){
+      clearInterval(count);
+      c.classList.add("hide");
+    }
+  },60)
+}
+counter();
+
 /* mouse-cursor event_________________________________*/
 const isTouchDevice = 'ontouchstart' in window;
 
